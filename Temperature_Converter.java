618ee183c58d3615ec6565055c9cc5fd2c3f1f1c
Papri Saha <paprisaha986@gmail.com>
2024-10-03 23:57:14 +0600

    Updated commit

---
+++ Temperature_Converter.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class Test {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Enter Celsius : ");
+            double celsius = scan.nextDouble();
+
+            double fahrenheit = 9 / 5.0 * celsius + 32; // F = 9/5 C + 32
+            System.out.println("Fahrenheit: " + fahrenheit);
+        }
+    }
+}
